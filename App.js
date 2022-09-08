047b83f7f11ba68bed881d03c2b0761b965ec8db
Soumya Xavier <denofsoumya@gmail.com>
2022-09-08 20:13:05 +0530

    committed

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Main from './screens/Main';
+
+export default function App() {
+  return (
+   <Main/>
+  );
+}
+
